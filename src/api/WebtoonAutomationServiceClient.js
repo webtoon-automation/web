a8917e4d28ffbeb57168e5101d1d1a2aa6f0b4b0
Joe Riess <joeriess94@gmail.com>
2020-05-12 19:36:31 -0400

    using file-saver modul

--- src/api/WebtoonAutomationServiceClient.js
+++ src/api/WebtoonAutomationServiceClient.js
@@ -1,29 +1,19 @@
-import Axios from "axios";
-import streamSave from "streamsaver";
+const saveAs = require("file-saver");
 
 export default class WebtoonAutomationServiceClient {
   constructor(baseURL) {
     this.baseURL = baseURL;
-    this.http = Axios.create({
-      baseURL,
-    });
   }
 
   async prepare(file) {
     const formData = new FormData();
     formData.append("file", file);
 
-    const { data } = await this.http.post("/v1/prepare", formData, {
-      responseType: "stream",
-      headers: {
-        "content-type": "multipart/form-data",
-      },
+    const response = await fetch(`${this.baseURL}/v1/prepare`, {
+      method: 'POST',
+      body: formData
     });
-
-    window.console.log(data);
-
-    data.pipe(streamSave.createWriteStream("file.zip"));
-
-    return data;
+    const blob = await response.blob();
+    saveAs(blob);
   }
 }
